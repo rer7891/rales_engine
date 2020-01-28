49fcb47f0b5a60010fea94570824eb9808279631
Becky Robran <rerobran@gmail.com>
2020-01-28 13:36:24 -0700

    Fixes factory spelling of folder

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :random_item, class: Item do
+  factory :item, class: Item do
     sequence(:name)   {|n| "#{Faker::Commerce.product_name} #{n}"}
     description       {Faker::Lorem.sentence}
     unit_price             {rand(100..10000)}
