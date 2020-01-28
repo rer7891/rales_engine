604b90181c45dc5b48489f7cc935b74e53fa4000
Becky Robran <rerobran@gmail.com>
2020-01-27 17:27:55 -0700

    Adds changes to the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_233506) do
+ActiveRecord::Schema.define(version: 2020_01_27_235348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,11 +34,11 @@ ActiveRecord::Schema.define(version: 2020_01_27_233506) do
   end
 
   create_table "invoices", force: :cascade do |t|
-    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "customer_id"
     t.bigint "merchant_id"
+    t.integer "status"
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
     t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
   end
