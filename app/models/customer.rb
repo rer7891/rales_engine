406c46246eaec7e2fba73a87b4b6148f738cedd2
Becky Robran <rerobran@gmail.com>
2020-01-28 11:45:10 -0700

    Adds destroy for dependents

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,5 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name
   validates_presence_of :last_name
 
-  has_many :invoices
+  has_many :invoices, dependent: :destroy
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,8 +4,8 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
 
-  has_many :transactions
-  has_many :invoice_items
+  has_many :transactions, dependent: :destroy
+  has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
 
   enum status: %w(shipped)
