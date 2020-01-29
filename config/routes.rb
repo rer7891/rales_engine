bb7c8ccaec55133214709874479908124dc4f317
Becky Robran <rerobran@gmail.com>
2020-01-28 20:23:30 -0700

    Updates factories with new naming

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+
   namespace :api do
     namespace :v1 do
+
+      resources :customers, only: [:index, :show]
+      resources :items, only: [:index, :show]
+      resources :invoices, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
+      resources :invoice_items, only: [:index, :show]
+
+      get '/merchants/most_revenue', to: 'merchants_revenue#index'
+
       resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index] 
+        resources :items, only: [:index, :show]
       end
     end
   end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
-  factory :random_transaction, class: Transaction do
+  factory :transactions, class: Transaction do
     credit_card_number           {rand(100..10000)}
     credit_card_expiration_date  {rand(100..10000)}
     result                       {"success"}
 
-    association   :invoice, factory: :random_invoice
+    association   :invoice, factory: :invoice
   end
 end
