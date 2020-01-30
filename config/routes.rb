7d452c3ab45676ccbc86d9aeeabd55c8c2cf564b
Becky Robran <rerobran@gmail.com>
2020-01-29 19:35:43 -0700

    updates routes to namespace merchant items

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       namespace :merchants do
+        get '/:merchant_id/items', to: 'items#index'
+        get '/:merchant_id/invoices', to: 'invoices#index'
         get '/find_all', to: 'find#index'
         get '/find', to: 'find#show'
         get '/random', to: 'random#show'
@@ -18,7 +20,6 @@ Rails.application.routes.draw do
       get '/merchants/most_revenue', to: 'merchants_revenue#index'
 
       resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index, :show]
       end
     end
   end
