3fb36e8ce59312d6d08ad7f7b832594d3500502e
Becky Robran <rerobran@gmail.com>
2020-01-30 14:48:36 -0700

    Converts unit_price to string in serializer

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,4 +1,8 @@
 class InvoiceItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :item_id, :invoice_id, :quantity, :unit_price
+  attributes :id, :item_id, :invoice_id, :quantity
+
+  attributes :unit_price do |invoice_item|
+    invoice_item.unit_price.to_s
+  end
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,10 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :description, :unit_price, :merchant_id
+  attributes :id, :name, :description, :merchant_id
+
+  attributes :unit_price do |item|
+    item.unit_price.to_s
+  end
 
   has_many :invoices
 end
