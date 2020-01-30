87dee32bc330e2edafbde6a43bd81c0aed48015b
Becky Robran <rerobran@gmail.com>
2020-01-29 19:10:38 -0700

    adds endpoints for merchant find all and random

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+
       namespace :merchants do
+        get '/find_all', to: 'find#index'
         get '/find', to: 'find#show'
+        get '/random', to: 'random#show'
       end
+
       resources :customers, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :invoices, only: [:index, :show]
