f166da9424571755960ab812156a69a479bcceec
Becky Robran <rerobran@gmail.com>
2020-01-29 19:34:53 -0700

    moves merchant items logic to merchant items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    if params[:merchant_id]
-      merchant = Merchant.find(params[:merchant_id])
-      render json: ItemSerializer.new(merchant.items)
-    else
       render json: ItemSerializer.new(Item.all)
-    end
   end
 
   def show
