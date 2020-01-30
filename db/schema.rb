4c36136e02ecdc3d693a440ea9be34b8162caee9
Becky Robran <rerobran@gmail.com>
2020-01-29 19:36:12 -0700

    updates transactions credit card to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_191739) do
+ActiveRecord::Schema.define(version: 2020_01_30_023139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2020_01_29_191739) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.bigint "credit_card_number"
+    t.string "credit_card_number"
     t.integer "credit_card_expiration_date"
     t.string "result"
     t.datetime "created_at", null: false
