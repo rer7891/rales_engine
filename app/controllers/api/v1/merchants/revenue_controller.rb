21bb22f8332253366cd2eb28f40ccf61c92f52a3
Becky Robran <rerobran@gmail.com>
2020-02-02 16:33:39 -0700

    reorders merchants revenue and ranks controllers

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::Merchants::RevenueController < ApplicationController
-  def index
-    render json: MerchantSerializer.new(Merchant.most_revenue_merchant(params[:quantity]))
-  end
-
-  def show
-    render json: RevenueSerializer.new(Merchant.revenue_date(params[:date]))
-  end
-end
