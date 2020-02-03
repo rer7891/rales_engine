60b3844cb028ae548ed9be49cd0ec33de8214f24
Becky Robran <rerobran@gmail.com>
2020-02-02 21:08:12 -0700

    cleans up business controller endpoints

--- app/controllers/api/v1/merchants/revenues_controller.rb
+++ app/controllers/api/v1/merchants/revenues_controller.rb
@@ -5,7 +5,10 @@ class Api::V1::Merchants::RevenuesController < ApplicationController
     end
 
     def show
-require "pry"; binding.pry
+      if params[:date] != nil
         render json: {data: {attributes: {revenue: Merchant.revenue_date_by_merchant(params[:date], params[:id])}}}
+      else
+        render json: {data: {attributes: {revenue: Merchant.most_revenue_by_merchant(params[:id])}}}
+      end
     end
 end
