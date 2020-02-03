15849c157157ac4be66b7a084bd48e24afcd0d85
Becky Robran <rerobran@gmail.com>
2020-02-02 21:09:08 -0700

    adds business endpoint queries

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,10 @@ class Item < ApplicationRecord
   def self.most_revenue(quantity)
     joins(invoices: [:invoice_items, :transactions]).select('items.*, sum(invoice_items.quantity) as total_sold').merge(Transaction.successful).group(:id).reorder(total_sold: :desc).limit(quantity)
   end
+
+  def self.best_day(id)
+    id = id.to_s
+   day = joins(invoices: [:invoice_items, :transactions]).select('items.*, invoices.created_at as best_day, sum(invoice_items.quantity) as total_sold').group('items.id, invoices.created_at').where('items.id = ?', id).merge(Transaction.successful).reorder('total_sold desc, best_day desc').limit(1).first
+   day.best_day.strftime("%Y-%m-%d")
+  end
 end
